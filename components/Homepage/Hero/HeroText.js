89c944fa3cabe37a6815b45c3d9add5d043ab7cb
Buddy Agyin <agyin3@gmail.com>
2020-11-02 10:33:37 -0700

    add styling for responsiveness

--- components/Homepage/Hero/HeroText.js
+++ components/Homepage/Hero/HeroText.js
@@ -2,16 +2,19 @@ import React from "react";
 
 const HeroText = () => (
   <div className="center-text hero-text-container">
-    <h1 className="hero-heading">PREMIUM SEATING</h1>
-    <p className="yellow-text hero-sub-heading">
-      THE BEST WAY TO ENJOY ALL THE EXCITEMENT
-    </p>
-    <p className="hero-text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ut nisi
-      explicabo repellendus, saepe quisquam eveniet, quas magni voluptatibus
-      vitae. Provident, laudantium corporis quam voluptate temporibus illo.
-    </p>
-    <a className="hero-link link-button">Contact Us</a>
+    <span>
+      <h1 className="hero-heading">PREMIUM SEATING</h1>
+      <p className="yellow-text hero-sub-heading">
+        THE BEST WAY TO ENJOY ALL THE EXCITEMENT
+      </p>
+      <p className="hero-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ut
+        nisi explicabo repellendus, saepe quisquam eveniet, quas magni
+        voluptatibus vitae. Provident, laudantium corporis quam voluptate
+        temporibus illo.
+      </p>
+      <a className="hero-link link-button">Contact Us</a>
+    </span>
   </div>
 );
 
